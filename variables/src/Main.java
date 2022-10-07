c4469ec043a665599496a6e9436c74b33f8f68b0
Mehmet <mehkara004@gmail.com>
2022-10-07 20:47:29 +0300

    commit file

---
+++ variables/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 11;
+        String yazdir ="kaç öğrenci var : ";
+
+        //System.out.println("ogrenci sayim = "+ogrenciSayisi);
+        System.out.println( yazdir + ogrenciSayisi);
+        System.out.println( yazdir + ogrenciSayisi);
+        System.out.println( yazdir + ogrenciSayisi);
+    }
+}
