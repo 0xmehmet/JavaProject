cab7d34fee8d92072aa272206298af9a20b29acf
Mehmet <100871805+0xmehmet@users.noreply.github.com>
2022-10-08 16:17:59 +0300

    Add files via upload

---
+++ forDongusu/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //loop demo
+               // 1 ;  2 ; 3
+          // 1 : sayaç kısımı 2 : şart kısımı 3:sayaçın kaçar kaçar artacağını belirttiğimiz yer
+        for (int i=0;i<=10;i++){ //i+=2
+            System.out.println(i);
+
+        }
+        System.out.println("dongu bitti");
+    }
+}
