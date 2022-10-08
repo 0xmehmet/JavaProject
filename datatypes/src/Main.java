2d538754381e09622dd8d854f15e28060d5edd82
Mehmet <mehkara004@gmail.com>
2022-10-08 11:39:09 +0300

    commit file

---
+++ datatypes/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // int tam sayıdır
+        //float ondalıklıdır ve 4 byte
+        //double ondalıklıdır ve 8 byte
+        int sayi = 11;
+        sayi = 122222222;
+        //char tek karakter tutmaya yarar tek tırnak ile gösteririz
+
+        char character = 'b';
+
+        //boolean doğru veya yanlışı
+
+        boolean dogruMu=false;
+
+
+    }
+}
