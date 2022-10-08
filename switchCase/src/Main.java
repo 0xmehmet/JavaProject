d0a38eeb7c8bb7ac19888293ef4149b8a9a0de8b
Mehmet <mehkara004@gmail.com>
2022-10-08 12:51:14 +0300

    switchcase file

---
+++ switchCase/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        char grade = 'Z';
+
+        switch (grade) {
+            case 'A':
+
+                System.out.println("ÇOK İYİ");
+                break;
+
+            case 'B':
+
+                System.out.println("İYİ");
+                break;
+            case 'C':
+
+                System.out.println("ORTA");
+                break;
+            case 'D':
+
+                System.out.println("KÖTÜ");
+                break;
+            case 'F':
+
+                System.out.println("KALDI");
+                break;
+            default: //yukarıdaki sayılardan başka tuşlama yapılırsa diye bir nevi else gibi
+                System.out.println("geçersiz harf kullanımı");
+
+
+                // kullandığım ide + format code
+                //ctr+shift+alt+l  intellij için geçerlidir.
+
+        }
+
+    }
+}
