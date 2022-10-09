4374aae548c697583f668f091ed3afb798c922f9
Mehmet <100871805+0xmehmet@users.noreply.github.com>
2022-10-09 12:24:32 +0300

    Add files via upload

---
+++ whileLoop/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i<10){
+            System.out.println(i);
+            i++;
+
+
+        }
+        System.out.println("loop sonu");
+
+
+        //Do-While LOOP
+        int j =1;
+        do {
+
+                System.out.println(j);
+                j++;
+
+
+            }while (j<10);
+
+
+
+
+    }
+}
